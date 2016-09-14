1f947e152736b45c2850c9772e8c2413ccd2674b
Alexander Glassford <a.b.glassford@gmail.com>
2016-09-14 11:10:38 -0600

    complete migration files

--- src/server/db/migrations/20160914105226_restaurants.js
+++ src/server/db/migrations/20160914105226_restaurants.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('restaurants', table => {
+    table.increments();
+    table.string('restaurant_name').notNullable().unique();
+    table.string('style').notNullable();
+    table.string('street').notNullable();
+    table.string('city').notNullable();
+    table.string('state').notNullable();
+    table.integer('zip').notNullable();
+    table.string('images').notNullable();
+    table.text('description').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('restaurants');
 };
--- src/server/db/migrations/20160914105233_reviews.js
+++ src/server/db/migrations/20160914105233_reviews.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('reviews', table => {
+    table.increments();
+    table.integer('rating').notNullable();
+    table.string('reviewer_name').notNullable();
+    table.date('date').notNullable();
+    table.text('text').notNullable();
+    table.integer('user_id').notNullable();
+    table.foreign('user_id').references('id').inTable('users');
+    table.integer('restaurant_id').notNullable();
+    table.foreign('restaurant_id').references('id').inTable('restaurants');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('reviews');
 };
