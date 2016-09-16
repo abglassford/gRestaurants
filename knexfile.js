2ff7db645c77c8afd283717ff30a036a4d1a1ed7
Alexander Glassford <a.b.glassford@gmail.com>
2016-09-16 13:39:45 -0600

    install pg

--- knexfile.js
+++ knexfile.js
@@ -10,8 +10,8 @@ module.exports = {
     }
   },
   production: {
-    client: 'postgresql',
-    connection: DATABASE_URL,
+    client: 'pg',
+    postgres:'postgres://gpqpqymbgfcugw:rvM5dmii49XbNkYjJYpIJDDPrf@ec2-54-243-63-130.compute-1.amazonaws.com:5432/d3gi2b3k7ag2p1',
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
     },
