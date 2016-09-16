19d197556e761429af21692764ec9ce193eff44c
Alex Nye <alnye655321@gmail.com>
2016-09-16 08:07:54 -0600

    validation almost finished

--- src/server/routes/validation.js
+++ src/server/routes/validation.js
@@ -1,16 +1,39 @@
 const knex = require('../db/knex');
 function verify(req, res, next) {
   //req.checkParams('id', 'Must be valid').notEmpty().isInt();
-  req.checkBody('restaurant_name', 'Restaurant name cannot be empty').notEmpty();
-  //req.checkBody('email', 'Must be a valid email').isEmail();
+  req.checkBody('name', 'Restaurant name cannot be empty').notEmpty();
+  req.checkBody('city', 'City name cannot be empty').notEmpty();
+  req.checkBody('state', 'State name cannot be empty').notEmpty();
+  // req.checkBody('cuisine', 'Cuisine cannot be empty').notEmpty();
+  //req.checkBody('images', 'Image must be valid URL').isAlpha();
+  // req.checkBody('description', 'Description cannot be empty').notEmpty();
+  req.checkBody('zip', 'Zip must in format of 80206-3452').notEmpty().len(10);
+  req.checkBody('street', 'Street name cannot be empty').notEmpty();
 
-  const errors = req.validationErrors();
-  if (errors) {
-    return res.status(400).json({
-      message: 'Validation failed',
-      failures: errors
-    });
-  } else {
+  //req.checkBody('email', 'Must be a valid email').isEmail();
+  const renderObject = {};
+  renderObject.errors = req.validationErrors();
+  // if (errors) {
+  //   return res.status(400).json({
+  //     message: 'Validation failed',
+  //     failures: errors
+  //   });
+  // }
+  if (renderObject.errors) {
+    renderObject.formValues = {
+      name: req.body.name,
+      city: req.body.city,
+      state: req.body.state,
+      cuisine: req.body.cuisine,
+      images: req.body.images,
+      description: req.body.description,
+      zip: req.body.zip,
+      street: req.body.street
+    };
+    console.log(renderObject.errors);
+    return res.render('new_restaurant', renderObject).status(400);
+  }
+  else {
     return next();
   }
 }
