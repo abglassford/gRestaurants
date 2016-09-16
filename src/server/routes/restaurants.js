da86d94bca267131f14f3d72c854c963815fe7f5
Tyler Maier <tyler_maier@yahoo.com>
2016-09-16 14:47:58 -0600

    populates map on /:id pages

--- src/server/routes/restaurants.js
+++ src/server/routes/restaurants.js
@@ -20,6 +20,16 @@ router.get('/', indexController.isAuthenticated, function (req, res, next) {
   });
 });
 
+// var restNameURI;
+// function encodeURIName (name) {
+//   restNameURI = encodeURI(name);
+// }
+
+function mapShow (name) {
+  const restNameURI = encodeURI(name);
+  return `https://www.google.com/maps/embed/v1/search?key=AIzaSyD3nHjd0_RGDNdjaWEqsfJpcNn7WD3osic&q=${restNameURI}`;
+}
+
 router.get('/new', (req, res, next) => {
   const renderObject = req.renderObject;
   res.render('new_restaurant', renderObject);
@@ -36,6 +46,7 @@ router.get('/:id', indexController.isAuthenticated, function (req, res, next) {
   .then((restaurant) => {
     renderObject.title = 'gRestaurants';
     renderObject.restaurants = restaurant;
+    renderObject.restaurants[0].map = mapShow(restaurant[0].restaurant_name);
   });
   promiseArray.push(restaurantPromise);
 
