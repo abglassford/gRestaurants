b46438deb1f48a7198e3c0bb1644b579de2d3984
Alexander Glassford <a.b.glassford@gmail.com>
2016-09-16 14:09:13 -0600

    a;lskdfj

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
     }
   },
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
