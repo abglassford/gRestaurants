91a691012f596d2e0b6b9b89e37bd871597a816d
Alexander Glassford <a.b.glassford@gmail.com>
2016-09-16 13:55:09 -0600

    try this again

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    postgres:'postgres://gpqpqymbgfcugw:rvM5dmii49XbNkYjJYpIJDDPrf@ec2-54-243-63-130.compute-1.amazonaws.com:5432/d3gi2b3k7ag2p1',
+    postgres: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
     },
